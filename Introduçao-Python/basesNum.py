8d69946da5c6c9c480da9f8cd4f7d2e4bae38f26
Pedro Henrique Gazola <ph-gazola@hotmail.com>
2022-08-17 19:02:46 -0300

    Transformar binario em decimal

--- Introduçao-Python/basesNum.py
+++ Introduçao-Python/basesNum.py
@@ -7,4 +7,12 @@ def TransformarDEC(num):
 
 print(TransformarDEC(int(input('Digite um número: '))))
 
-
+def TransformarBIN(num):
+    decimal = 0
+    expoente = 0
+    while(num > 0):
+        resto = num % 10
+        decimal = decimal + resto * 2 ** expoente
+        expoente = expoente + 1
+        num = num // 10
+    return decimal
