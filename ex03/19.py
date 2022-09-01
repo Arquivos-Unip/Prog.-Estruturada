97c995561d83e68d8fb1de5aade9b4f9d22bfc40
Pedro Gazola <ph-gazola@hotmail.com>
2022-09-01 18:55:56 -0300

    Alteraçao no projeto 19

--- ex03/19.py
+++ ex03/19.py
@@ -1,10 +1,25 @@
-numero = int(input("Digite um numero menor q 1000: "))
 
-numeroStr =str(numero)
-if (numero > 1000):
-    print("Numero invalido!")
-else:
-    centenas = (numero) // (10 ** (len(numeroStr)- 1))
-    dezenas = ((numero - (centenas * 100)) // 10)
-    unidade = ((numero - (centenas * 100)) - (dezenas * 10))
-    print(numero, "= ", centenas, ' centenas, ', dezenas, " dezenas e", unidade, " unidades." )
+# if (numero > 1000):
+#     print("Numero invalido!")
+# else:
+#     centenas = (numero) // (10 ** (len(numeroStr)- 1))
+#     dezenas = ((numero - (centenas * 100)) // 10)
+#     unidade = ((numero - (centenas * 100)) - (dezenas * 10))
+#     print(numero, "= ", centenas, ' centenas, ', dezenas, " dezenas e", unidade, " unidades." )
+
+numero = int(input("Digite um numero inteiro: "))
+numeroInicial = numero
+numeroStr = str(numero)
+casas = len(numeroStr)
+fragmentado = []
+
+while(casas > 0):
+    numeroFrag = (numero) // (10 ** (len(numeroStr)- 1))
+    fragmentado.append(numeroFrag)
+    numero = numero - (numeroFrag * (10 ** (len(numeroStr)- 1)))
+    casas = casas - 1
+    numeroStr = str(numero)
+
+print("Numero digitado: ",numeroInicial,"\nNumero dividido em casas decimais: ",fragmentado)
+
+
