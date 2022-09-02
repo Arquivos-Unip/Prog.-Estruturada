9023c756f491d13c8c17d5849a5de05a8c7ac9d8
Pedro Gazola <ph-gazola@hotmail.com>
2022-09-01 23:39:45 -0300

    Alterçao exercicio tinta2

--- ex02/tinta2.py
+++ ex02/tinta2.py
@@ -1,16 +1,25 @@
-metrosQuadrados2= float(input("Qual a medida da area a ser pintada em m2: "))
+metrosQuadrados2= int(input("Qual a medida da area a ser pintada em m2: "))
+metrosQuadrados2Str = str(metrosQuadrados2)
+newMetrosQuadrados2 = metrosQuadrados2 * 1.1
+preço18 = 80
+preço36 = 25
+coberturaLata18 = 108
+coberturaLata36 = 21.6
 
-def latasTinta2(metrosQuadrados2): 
-    preço18 = 80
-    preço36 = 25
-    coberturaLata18 = 108
-    coberturaLata36 = 21.6
-    if(metrosQuadrados2 <= coberturaLata36): print("É necessario apenas uma lata pequena\nO preço total é: ", preço36)
+156
+
+if(newMetrosQuadrados2 <= coberturaLata36): print("É necessario apenas uma lata pequena\nO preço total é: ", preço36)
+else:
+    latasNecessarias36 = 0
+    latasNecessarias18 = 0
+    latasNecessarias18 = newMetrosQuadrados2 // 108
+    latasNecessarias36 = (newMetrosQuadrados2 - (latasNecessarias18 * 108)) / 21.6
+    if(latasNecessarias36 % 1 == 0):
+        latasNecessarias36 = latasNecessarias36
     else:
-        latasNecessarias36 = (metrosQuadrados2 // coberturaLata36) + 1
-        latasNecessarias18 = (metrosQuadrados2 // coberturaLata18) + 1
-        print("Serao necessarias ", latasNecessarias36, " latas de 3,6L", "\nO preço total é: ", (latasNecessarias36 * preço36))
-        print("Serao necessarias: ", latasNecessarias18, "\nO preço total é: ", (preço18 *  latasNecessarias18)
-)
+        latasNecessarias36 = (latasNecessarias36 // 1) +1
 
-latasTinta2(metrosQuadrados2)
+total18 = latasNecessarias18 * preço18
+total36 = latasNecessarias36 * preço36
+total = total18 + total36
+print("Serao necessarias:", latasNecessarias18,"lata(s) de 18L e", latasNecessarias36,"galão(s) de 3,6L","\nTotal a ser pago:",total )
